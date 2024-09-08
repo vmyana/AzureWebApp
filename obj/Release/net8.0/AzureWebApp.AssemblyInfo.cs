a619729eb4f1094e895fa7b3f8bdb61b679c6457
Krish Krishna <krikrishna@deloitte.com>
2024-09-08 17:24:53 +0530

    prod changes

--- obj/Release/net8.0/AzureWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/AzureWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00b6f002a7d5b8c6c458816c3febc73b7e3759e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
